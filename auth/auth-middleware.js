fa9a42919c1eae2381153de7a4d25e89821f70b0
Julia Vu <juliadvu@gmail.com>
2020-05-23 18:08:40 -0500

    updates to auth-middleware

--- auth/auth-middleware.js
+++ auth/auth-middleware.js
@@ -1,22 +1,23 @@
 const jwt = require("jsonwebtoken");
 
-//verify users are logged in
-
+// verify users are logged in
 module.exports = (req, res, next) => {
+  const token = req.headers.authorization;
 
-    const token = req.headers.authorization;
-    if (token) {
-        const secret = process.env.JWT_SECRET || "top secret!";
+  if (token) {
+    const secret = process.env.JWT_SECRET || "top secret!";
 
-        jwt.verify(token, secret, (error, decodedToken) => {
-            if (error) {
-                res.status(401).json({ message: "you cannot pass!" }); // the token is invalid
-
-                req.jwt = decodedToken;
-                next();  // the token is good
-            }
-        });
-    } else {
-        res.status(400).json({ message: "Please provide the authentication information" });
-    }
+    jwt.verify(token, secret, (error, decodedToken) => {
+      if (error) {
+        // the token is invalid
+        res.status(401).json({ message: "sorry, access denied." });
+      } else {
+        // the token is good
+        req.jwt = decodedToken;
+        next();
+      }
+    });
+  } else {
+    res.status(400).json({ message: "Please provide the authentication information" });
+  }
 };
