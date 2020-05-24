2c580281b004d8159003dbee11ec3881d9cd056f
Julia Vu <juliadvu@gmail.com>
2020-05-23 21:09:34 -0500

    made functions for users-model. login and register endpoints working on postman

--- users/users-model.js
+++ users/users-model.js
@@ -1,6 +1,29 @@
-// const db = require('../database/dbConfig');
+const db = require('../database/dbConfig');
 
-// module.exports ={
-//     add,
+module.exports ={
+    add,
+    findById, 
+    find, 
+    findBy
 
-// }
+}
+
+function findBy(filter){
+    return db('users').where(filter);
+}
+
+
+function find(){
+    return db('users').select( 'username', 'password', 'email')
+}
+
+function add(user){
+    return db('users').insert(user)
+    .then(ids=>{
+        return db('users').where({id: ids[0]}).first()
+    })
+}
+
+function findById(id){
+    return db('users').where({id}).first();
+}
