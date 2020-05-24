bf0ad3d7aefda95198df71fa079959d3d13b6a7c
Julia Vu <juliadvu@gmail.com>
2020-05-23 23:38:36 -0500

    working on recipes-router

--- recipes/recipes-router.js
+++ recipes/recipes-router.js
@@ -1,1 +1,19 @@
+const express = require('express');
+const Recipes = require('./recipes-model');
+
+const router = require('express').Router();
+
+//GET request-returns array for recipes from login token
+router.get('/',(req,res)=>{
+    Recipes.find()
+    .then(recipes=>{
+        res.status(200).json(recipes)
+    })
+    .catch(err=>{
+        console.log(err)
+        res.status(500).json({errorMessage: "Sorry, encountered an error in retreiving recipes."})
+    })
+})
+
+
 module.exports = router;
