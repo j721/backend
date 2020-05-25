66e80cf652f73722ea107620a687501750dd06c9
Julia Vu <juliadvu@gmail.com>
2020-05-25 16:47:12 -0500

    working on testing in server.spec file

---
+++ api/server.spec.js
@@ -0,0 +1,45 @@
+const request = require('supertest');
+const server = require('./server');
+
+const db = require('../database/dbConfig');
+
+// beforeEach(() => {
+//     return db.migrate
+//       .rollback()
+//       .then(() => db.migrate.latest())
+//       .then(() => db.seed.run());
+//   });
+
+
+describe("server", () => {
+    describe("test environment", () => {
+        it("test environment should be testing", () => {
+            expect(process.env.DB_ENV).toBe("testing");
+        })
+
+        it("runs the server", () => {
+            expect(true).toBe(true);
+        })
+    })
+})
+
+
+describe("GET /", () => {
+    it("should return http status code 200 OK", () => {
+        return supertest(server)
+            .get("/")
+            .then(response => {
+                expect(response.status).toBe(200);
+            })
+    });
+
+    it("should return { api: 'it's working, it's working! }", () => {
+        return supertest(server)
+            .get("/")
+            .then(response => {
+                expect(response.body).toEqual({api: "it's working, it's working!"});
+                expect(response.body.api).toBeDefined();
+                expect(response.body.api).toBe("it's working, it's working!");
+            });
+    });
+});
