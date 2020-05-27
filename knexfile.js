d3481c4253b50711035c1826f07f3853754448a5
Julia Vu <juliadvu@gmail.com>
2020-05-27 12:08:20 -0500

    deleted pool

--- knexfile.js
+++ knexfile.js
@@ -44,11 +44,8 @@ module.exports = {
     seeds: {
       directory: "./database/seeds",
     },
-    pool: {
-      afterCreate: (conn, done) => {
-          conn.run('PRAGMA foreign_keys = ON', done);
-      }
-  },
+  
+  
 }
 
 };
